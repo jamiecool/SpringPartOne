35eaece3b4f0709607fe53b79d5a7795a0d0a82a
Chupin Pevel <pavel.chupin@gmail.com>
2020-07-14 19:37:38 +0700

    ДЗ5

---
+++ home_work_lesson5/src/test/java/com/geekbrains/july/market/julymarket/JulyMarketApplicationTests.java
@@ -0,0 +1,13 @@
+package com.geekbrains.july.market.julymarket;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class JulyMarketApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
