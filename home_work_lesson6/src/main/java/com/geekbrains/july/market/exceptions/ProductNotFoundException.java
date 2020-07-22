913a679347482297d1f0b617f3f3eb75de5d3520
Chupin Pevel <pavel.chupin@gmail.com>
2020-07-22 14:17:03 +0700

    Выложил не те исходники по ДЗ6

--- home_work_lesson6/src/main/java/com/geekbrains/july/market/exceptions/ProductNotFoundException.java
+++
@@ -1,7 +0,0 @@
-package com.geekbrains.july.market.exceptions;
-
-public class ProductNotFoundException extends RuntimeException {
-    public ProductNotFoundException(String message) {
-        super(message);
-    }
-}
